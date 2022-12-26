e72d75874706429a6c5683acc9eaeccc9985bf02
Thao Dang <dangthao.stuk14@gmail.com>
2022-12-26 16:45:58 +0700

    update docker

--- README.md
+++ README.md
@@ -2,7 +2,33 @@
 - Nestjs
 - Protobuf
 - Mono-repo 
-## Run
+## Code generation
 ```
 npm run proto:all
+```
+
+## How to use
+
+```
+#!/bin/bash
+
+docker stop api
+docker stop user-svc
+docker stop doctor-svc
+
+docker rm api
+docker rm user-svc
+docker rm doctor-svc
+
+docker rmi api:v1
+docker rmi user-svc:v1
+docker rmi doctor-svc:v1
+
+docker build -t api:v1 -f api/Dockerfile .
+docker build -t user-svc:v1 -f services/user-svc/Dockerfile .
+docker build -t doctor-svc:v1 -f services/doctor-svc/Dockerfile .
+
+docker run -itd --net=host -p 8080:3000 --name=api api:v1
+docker run -itd --net=host --name=user-svc user-svc:v1
+docker run -itd --net=host --name=doctor-svc doctor-svc:v1
 ```
--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 docker stop api
-docker stop service_user
-docker stop service_doctor
+docker stop user-svc
+docker stop doctor-svc
 
 docker rm api
-docker rm service_user
-docker rm service_doctor
+docker rm user-svc
+docker rm doctor-svc
 
 docker rmi api:v1
-docker rmi service_user:v1
-docker rmi service_doctor:v1
+docker rmi user-svc:v1
+docker rmi doctor-svc:v1
 
 docker build -t api:v1 -f api/Dockerfile .
-docker build -t service_user:v1 -f services/user/Dockerfile .
-docker build -t service_doctor:v1 -f services/doctor/Dockerfile .
+docker build -t user-svc:v1 -f services/user-svc/Dockerfile .
+docker build -t doctor-svc:v1 -f services/doctor-svc/Dockerfile .
 
 docker run -itd --net=host -p 8080:3000 --name=api api:v1
-docker run -itd --net=host --name=service_user service_user:v1
-docker run -itd --net=host --name=service_doctor service_doctor:v1
+docker run -itd --net=host --name=user-svc user-svc:v1
+docker run -itd --net=host --name=doctor-svc doctor-svc:v1
