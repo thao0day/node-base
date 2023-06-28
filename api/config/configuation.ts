52b41df541ed18297f73717ade9bec9f8e4ec024
Thao Dang <thao.dang@vsee.com>
2023-06-28 10:48:34 +0700

    update

--- api/config/configuation.ts
+++ api/config/configuation.ts
@@ -1,14 +1,29 @@
-import { readFileSync } from 'fs';
-import * as yaml from 'js-yaml';
-import { join } from 'path';
-import { ConfigModule } from '@nestjs/config';
-ConfigModule.forRoot({
-  isGlobal: true,
-});
-const YAML_CONFIG = process.env.CONFIG_PATH
-  ? join(process.cwd(), '../', process.env.CONFIG_PATH || '', 'api.yaml')
-  : join(__dirname, 'api.yaml');
+// import { readFileSync } from 'fs';
+// import * as yaml from 'js-yaml';
+// import { join } from 'path';
+// import { ConfigModule } from '@nestjs/config';
+
+// ConfigModule.forRoot({
+//   isGlobal: true,
+// });
+
+// const YAML_CONFIG = process.env.CONFIG_PATH
+//   ? join(process.cwd(), '../', process.env.CONFIG_PATH || '', 'api.yaml')
+//   : join(__dirname, 'api.yaml');
 
 export default () => {
-  return yaml.load(readFileSync(YAML_CONFIG, 'utf8')) as Record<string, any>;
+  return {
+    http: {
+      host: process.env.API_HTTP_HOST || '0.0.0.0',
+      port: parseInt(process.env.API_HTTP_PORT, 10) || 3000,
+    },
+    services: {
+      user: {
+        host: process.env.USER_SVC_GRPC_HOST || '0.0.0.0',
+        port: parseInt(process.env.USER_SVC_GRPC_PORT, 10) || 50053,
+      },
+    },
+  };
 };
+
+// return yaml.load(readFileSync(YAML_CONFIG, 'utf8')) as Record<string, any>;
--- services/user/config/configuation.ts
+++ services/user/config/configuation.ts
@@ -1,15 +1,32 @@
-import { readFileSync } from 'fs';
-import * as yaml from 'js-yaml';
-import { join } from 'path';
-import { ConfigModule } from '@nestjs/config';
-ConfigModule.forRoot({
-  isGlobal: true,
-});
+// import { readFileSync } from 'fs';
+// import * as yaml from 'js-yaml';
+// import { join } from 'path';
+// import { ConfigModule } from '@nestjs/config';
+// ConfigModule.forRoot({
+//   isGlobal: true,
+// });
 
-const YAML_CONFIG = process.env.CONFIG_PATH
-  ? join(process.cwd(), '../', process.env.CONFIG_PATH || '', 'user.yaml')
-  : join(__dirname, 'user.yaml');
+// const YAML_CONFIG = process.env.CONFIG_PATH
+//   ? join(process.cwd(), '../', process.env.CONFIG_PATH || '', 'user.yaml')
+//   : join(__dirname, 'user.yaml');
 
 export default () => {
-  return yaml.load(readFileSync(YAML_CONFIG, 'utf8')) as Record<string, any>;
+  return {
+    grpc: {
+      host: process.env.USER_SVC_GRPC_HOST || '0.0.0.0',
+      port: parseInt(process.env.USER_SVC_GRPC_PORT, 10) || 50053,
+    },
+    db: {
+      type: process.env.USER_SVC_DB_TYPE as any,
+      host: process.env.USER_SVC_DB_HOST,
+      port: parseInt(process.env.USER_SVC_DB_PORT, 10) || 5432,
+      database: process.env.USER_SVC_DB_DATABASE,
+      username: process.env.USER_SVC_DB_NAME,
+      password: process.env.USER_SVC_DB_PASSWORD,
+      ssl: process.env.USER_SVC_DB_SSL === 'true',
+      synchronize: process.env.USER_SVC_DB_SYNCHRONIZE === 'true',
+      logging: process.env.USER_SVC_DB_LOGGING === 'true', // ['error', 'warn']
+      autoLoadEntities: true,
+    },
+  };
 };
